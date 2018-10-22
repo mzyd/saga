217091e573b797c9963eb5d696fd2ee115380e88
mzy <mzy3991@gmail.com>
2018-10-22 18:39:49 +0800

    complete chapter seven

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers/'
 import createSagaMiddleware from 'redux-saga'
 import { helloSaga } from './sagas'
+import axios from 'axios'
 
 const sagaMiddleware = createSagaMiddleware()
 
@@ -34,3 +35,26 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+function * gen() {
+  var a = yield axios.get('https://jsonplaceholder.typicode.com/posts')
+  console.log( "a: ", a )
+  var b = yield axios.get('https://jsonplaceholder.typicode.com/users')
+  console.log( "b: ", b )
+  return 'end'
+}
+
+run(gen)
+
+function run(generator) {
+  var gen = generator()
+  function handle(yielded) {
+    if (!yielded.done) {
+      yielded.value.then(res => {
+        return handle(gen.next(res))
+      })
+    }
+  }
+  return handle(gen.next())
+}
